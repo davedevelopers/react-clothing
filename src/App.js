590ed088db91ac1d8123f64d790721e4a5475d3e
davedeveloper <ddave2001@gmail.com>
2021-01-17 15:24:57 +0530

    Adding Header component with navigation

--- src/App.js
+++ src/App.js
@@ -4,13 +4,17 @@ import {Route, Switch} from 'react-router-dom'
 
 import Homepage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
+import Header from './components/Header/header.component.jsx'
 
 function App() {
   return (
-    <Switch>
-      <Route exact path='/' component={Homepage}/>
-      <Route exact path='/shop' component={ShopPage}/>
-    </Switch>
+    <React.Fragment>
+      <Header />
+      <Switch>
+        <Route exact path='/' component={Homepage}/>
+        <Route exact path='/shop' component={ShopPage}/>
+      </Switch>
+    </React.Fragment>
   );
 }
 
